1e55702550defc2c087f712919054a981c045557
hoomi <hoomi2010@gmail.com>
2013-11-20 17:27:44 +0000

    Initial Commit

---
+++ OpenGL_Tutorial1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Tutorial1
+//
+//  Created by Hooman Ostovari on 20/11/2013.
+//  Copyright (c) 2013 Hooman Ostovari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
